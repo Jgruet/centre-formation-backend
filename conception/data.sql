3a05f600a0950c3e325a620fdd266c544d455783
Jonathan Gruet <jon.gruet@yahoo.fr>
2020-08-05 16:00:25 +0200

    remplissage table formations

---
+++ conception/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Formations (titre, duree, date_debut, date_fin, description, lieu, type) VALUES
+('DWWM', 390, '2020-03-16', '2020-08-13', 'Formation pour devenir développeur web ou web mobile, comme tu veux', 'Bordeaux', 'À distance'),
+('CDA', 520, '2020-01-18', '2020-07-22', 'Formation concepteur développeur d application', 'Paris', 'Présentielle');
