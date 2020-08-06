fdf49066581d86b2f9b5b18bcd97d81c606e68be
Grégory Vandevraye <gvandevray@numericable.fr>
2020-08-06 15:34:40 +0200

    verif inscription

--- models/utilisateursDAO.js
+++ models/utilisateursDAO.js
@@ -2,4 +2,10 @@ const { DAO } = require('./dao');
 
 const utilisateursDAO = new DAO ('utilisateurs');
 
+utilisateursDAO.checkEmail = async (mail) => {
+    const sql = 'SELECT mail FROM utilisateurs WHERE mail = ?';
+    const result = await utilisateursDAO.query(sql, [mail]);
+    return result[0][0];
+};
+
 module.exports = utilisateursDAO;
--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var utilisateursDAO = require('../models/utilisateursDAO');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
@@ -9,17 +10,33 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const user = {
-        nom: req.body.nom,
-        prenom: req.body.prenom,
-        roleId: 3,
-        mail: req.body.mail,
-        mdp: req.body.mdp
+
+    const test = req.body.mail.match(/^[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+$/i);
+    const test2 = req.body.mdp.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-!"§$%&/()=?+*~#'_:.,;]).{8,}$/);
+    const checkEmail = await utilisateursDAO.checkEmail(req.body.mail);
+    try {
+        if (test && test2 && checkEmail == undefined) {
+            const hash = await bcrypt.hash(req.body.mdp, 2);
+            const user = {
+                nom: req.body.nom,
+                prenom: req.body.prenom,
+                roleId: 3,
+                mail: hash,
+                mdp: req.body.mdp
+            }
+            let result = await utilisateursDAO.insertOne(user);
+            console.log(result.affectedRows);
+            // si === 1, l'insertion est un succès
+            console.log('ok vous êtes inscrit');
+            //res.send(result.affectedRows);
+
+        }
+    } catch (err) {
+        console.log(err);
     }
-    let result = await utilisateursDAO.insertOne(user);
-    console.log(result.affectedRows);
-    // si === 1, l'insertion est un succès
-    res.send(result.affectedRows);
+
+
+
 })
 
 module.exports = router;
